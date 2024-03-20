b4031c4b8dcbb6e8766292eb54a7863e8d7a68cd
SanketPatole <69233883+SanketPatole@users.noreply.github.com>
2024-03-20 23:21:39 +0530

    Updated code

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,78 @@ import os
 import PyPDF2 as pdf
 from dotenv import load_dotenv
 import json
+import openai
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains.question_answering import load_qa_chain
+from langchain.output_parsers import ResponseSchema, StructuredOutputParser
+from langchain.document_loaders import CSVLoader, PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
+
+class GenAI_Wrpapper:
+	def __init__(self, chat_client='chatgpt3.5'):
+		if chat_client == 'chatgpt3.5':
+			self.chat_client = ChatOpenAI(model="gpt-3.5-turbo")
+			self.embedding = OpenAIEmbeddings()
+		elif chat_client == 'chatgpt3.5turbo':
+			self.chat_client = ChatOpenAI(model="gpt-3.5-turbo-instruct")
+			self.embedding = OpenAIEmbeddings()
+		elif chat_client == 'chatgpt4':
+			self.chat_client = ChatOpenAI(model="gpt-4")
+			self.embedding = OpenAIEmbeddings()
+	
+	def get_document_splits(self, file_data, chunk_size=1500, chunk_overlap=150):
+		splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+		splits = splitter.split_text(file_data)
+		return splits
+
+	def create_vectordb_from_document_splits(self, document_splits, embedding):
+		return FAISS.from_textx(document_splits, embedding=embedding)
+	  
+	def get_output_parser(self, ):
+		strengths = ResponseSchema(name="strengths", description="Strengths of the candidate")
+		weaknesses = ResponseSchema(name="weaknesses", description="Weaknesses of the candidate.")
+		summary = ResponseSchema(name="summary", description="Short summary of the analysis mentioning eligibility of the candidate.")
+		is_shortlisted = ResponseSchema(name="is_shortlisted", description="Do candidate's skills match with required skills? (Yes/No)")
+		return StructuredOutputParser.from_response_schemas([strengths, weaknesses, summary, is_shortlisted])
+
+	def get_prompt_template(self, ):
+		prompt_template_text = """
+		You will be provided with a Job Description enclosed within {delimiter_job_description} delimiter.
+		You'll also be provided with skills of the candidate enclosed within {delimiter_skills} delimiter.
+
+		Depending upon these two inputs, please provide the skill gap analysis for the employer to short-list resume.
+		Please make sure that the analysis is concise and to the point.
+
+		{delimiter_skills}{context}{delimiter_skills}
+
+		{delimiter_job_description}{job_description}{delimiter_job_description}
+
+		{instructions}
+		"""
+		return PromptTemplate(template=prompt_template_text,
+							input_variables=["context", "job_description", "delimiter_skills", "delimiter_job_description", "instructions"])
+
+	def get_qa_chain(self, chat_client, prompt):
+		return load_qa_chain(llm=chat_client, chain_type="stuff", prompt=prompt)
+		
+	def run_qa_chain(self, resume_text, job_description_text, chat_client):
+		query = "What are the skills and educational qualifications of the candidate?"
+		prompt = self.get_prompt_template()
+		output_parser = self.get_output_parser()
+		instructions = output_parser.get_format_instructions()
+		document_splits = self.get_document_splits(resume_text, chunk_size=1500, chunk_overlap=150)
+		vectordb = self.create_vectordb_from_document_splits(document_splits, embedding)
+		skills = vectordb.similarity_search(query, k=2)
+		chain = self.get_qa_chain(chat_client, prompt)
+		prompt_inputs = {"input_documents": skills, "job_description": job_description_text,
+					   "delimiter_skills": "###", "delimiter_job_description": "$$$", "instructions": instructions}
+		response = chain(prompt_inputs, return_only_outputs=True)
+		response_dict = output_parser.parse(response["output_text"])
+		return response_dict
 
 class Page:
 	def __init__(self):
@@ -12,6 +83,7 @@ class Page:
 		self.resume_content = ""
 		self.jd_content = None
 		self.submit_object = None
+		self.gen_ai_wrapper_object = GenAI_Wrpapper()
 	
 	def create_header(self, displayText="Header"):
 		st.header(displayText)
@@ -57,7 +129,8 @@ class Page:
 			if len(self.jd_content.strip()) == 0:
 				self.create_error_message(displayText="Please provide the job description.")
 			if len(self.resume_content.strip()) > 0 and len(self.jd_content.strip()) > 0:
-				self.create_subheader(displayText=self.resume_content)
+				response = self.gen_ai_wrapper_object.run_qa_chain(self.resume_content, self.jd_content, 'chatgpt3.5')
+				self.create_subheader(displayText=response)
 	
 page = Page()
 page.create_page()
