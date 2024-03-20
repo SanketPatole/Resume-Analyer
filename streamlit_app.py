4948c9804cadbba90bd45d59ebc7915c96303bca
SanketPatole <69233883+SanketPatole@users.noreply.github.com>
2024-03-20 22:26:01 +0530

    Fixed bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,39 +6,54 @@ from dotenv import load_dotenv
 import json
 
 
-def input_pdf_text(uploaded_file):
-    reader=pdf.PdfReader(uploaded_file)
-    text=""
-    for page in range(len(reader.pages)):
-        page=reader.pages[page]
-        text+=str(page.extract_text())
-    return text
-
-st.header("Upload your resume in PDF format.")
-uploaded_file = st.file_uploader("Choose a file...", type="pdf")
-
-text = ""
-
-if uploaded_file is not None:
-	try:
-		text=input_pdf_text(uploaded_file)
-	except:
-		st.error('The pdf file is corrupted.', icon="🚨")
-
-st.header("Enter the job description")
-job_description = st.text_area("Paste the job description here...", height=150)
-
-submit = st.button("Submit")
-
-if submit:
-	if len(text.strip()) == 0:
-		st.error('Please provide a valid resume.', icon="🚨")
-	if len(job_description.strip()) == 0:
-		st.error('Please provide the job description.', icon="🚨")
-	if len(text.strip()) > 0 and len(job_description.strip()) > 0:
-		st.subheader(text)
-
-#st.subheader(text)
+class Page:
+	def __init__(self):
+		self.resume_object = None
+		self.resume_content = ""
+		self.jd_content = None
+		self.submit_object = None
 	
+	def create_header(self, displayText="Header")
+		st.header(displayText)
+		
+	def create_input_text(displayText="Input", height=150):
+		return st.text_area(displayText, height=height)
+	
+	def create_error_message(self, displayText="Error")
+		st.error(displayText, icon="🚨")
+	
+	def create_file_widget(self, displayText="Choose a file...")
+		return st.file_uploader(displayText=displayText, type=fileType)
+	
+	def create_submit_button(self, displayText="Submit")
+		return st.button(displayText)
 
+	def read_pdf_file(self):
+		reader=pdf.PdfReader(self.resume_object)
+		self.resume_content = ""
+		for page in range(len(reader.pages)):
+			page = reader.pages[page]
+			self.resume_content += str(page.extract_text())
+		
+	def create_page():
+		self.create_header(displayText="Upload your resume in PDF format.")
+		self.resume_object = create_file_widget()
+		self.create_header(displayText="Upload your resume in PDF format.")
+		self.jd_content = self.create_input_text(displayText="Paste the job description here...", height=150)
+		self.submit = self.create_submit_button(displayText="Submit")
+		
+		if self.resume_object is not None:
+			try:
+				read_pdf_file()
+			except:
+				self.create_error_message(displayText="The pdf file is corrupted.")
+		
+		if self.submit_object is not None:
+			if len(self.resume_content.strip()) == 0:
+				self.create_error_message(displayText="Please provide a valid resume.")
+			if len(self.jd_content.strip()) == 0:
+				self.create_error_message(displayText="Please provide the job description.")
+			if len(self.resume_content.strip()) > 0 and len(self.jd_content.strip()) > 0:
+				st.subheader("Sumitted")
+	
 
